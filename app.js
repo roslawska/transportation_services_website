cd3ebb9edd5aee65b3d2e8906033f56f7caf0ee5
Roksana <roksana.roslawska@gmail.com>
2020-12-02 00:14:07 +0100

    Added carousel for gallery photos
    some minor fixes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+// const gallery = document.querySelector('.services .carousel');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -22,4 +23,9 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
+
+});
+
+$(document).ready(function () {
+	$('.carousel').owlCarousel();
 });
